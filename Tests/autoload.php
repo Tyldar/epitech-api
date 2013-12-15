bc34a539b01aca15f65a23e745d4c4f783d318b7
Raphy <raphythegeek@gmail.com>
2013-12-15 21:52:10 +0100

    Move the source files in the directory Sources

--- Tests/autoload.php
+++ Tests/autoload.php
@@ -1,8 +1,7 @@
 <?php
 function __autoload($class)
 {
-    $root = dirname(dirname(__FILE__));
-    $file = str_replace('EpitechAPI', $root, str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php');
+    $file = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'Sources'.substr(str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php', 10);
     if (file_exists($file))
         require_once($file);
 }
